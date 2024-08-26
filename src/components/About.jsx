b8eb8ba45c922a69abf193221c74d25caf3fc72d
isveckrali <mehmet.can.seyhan1@gmail.com>
2024-08-26 19:00:00 +0200

    about section added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import aboutSvg from '../assets/about.svg'
+import SectionTitle from './SectionTitle'
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section className='bg-white py-20'
+    id='about'>
+      <div className='align-element grid md:grid-cols-2
+      items-center gap-16'>
+        <img src={aboutSvg} className='w-full h-64'/>
+        <article>
+          <SectionTitle text='code and coffee' />
+          <p className='text-slate-600 mt-8 leading-loose'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque ipsum consectetur hic facere nobis ipsam consequatur doloremque! Ad, dolore accusantium. Possimus doloremque eveniet unde, non totam sit sed laudantium hic.
+          </p>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
     <section className='bg-emerald-100 py-24  '>
       <div className=' mx-auto max-w-7xl  px-8  grid  md:grid-cols-2 items-center gap-8'>
         <article>
-          <h1 className='text-7xl font-bold tracking-wider'>I'm John</h1>
+          <h1 className='text-7xl font-bold tracking-wider'>I'm Mehmet Can</h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
-            Front-end developer
+            Full-stack developer
           </p>
           <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>
             turning ideas into interactive reality
