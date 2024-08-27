3ad261ccb5383d5dcc42ebd6193c2f45460a9948
isveckrali <mehmet.can.seyhan1@gmail.com>
2024-08-27 20:26:57 +0200

    done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import ProjectsCard from './ProjectsCard';
+import { projects } from '../data';
+import SectionTitle from './SectionTitle';
 
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Projects
+    <section className='py-20 align-element' id='projects'>
+      <SectionTitle text='web creations' />
+      <div className='py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8'>
+        {projects.map((project) => {
+          return <ProjectsCard key={project.id} {...project} />;
+        })}
+      </div>
+    </section>
+  );
+};
+export default Projects;
